c59bb8961eea6e247d728a85519ea275dc004f61
JL-OLEMAR <joseluis19963@gmail.com>
2023-08-30 11:11:39 -0500

    feat: Add Layout

---
+++ components/layouts/Layout.tsx
@@ -0,0 +1,24 @@
+import Head from 'next/head'
+import { Box } from '@mui/material'
+
+interface Props {
+  title?: string
+  children: React.ReactNode
+}
+
+export function Layout({ title = 'OpenJira', children }: Props) {
+  return (
+    <Box sx={{ flexFlow: 1 }}>
+      <Head>
+        <title>{title}</title>
+      </Head>
+
+      {/* Navbar */}
+      {/* Sidebar */}
+
+      <Box sx={{ padding: '10px 20px' }}>
+        {children}
+      </Box>
+    </Box>
+  )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,12 @@
 import { Typography } from '@mui/material'
+import { Layout } from '../components/layouts'
 
 export default function Home() {
   return (
-    <Typography variant='h1' color='primary'>Hello world</Typography>
+    <Layout>
+      <Typography variant='h1' color='primary'>
+        Hola Mundo
+      </Typography>
+    </Layout>
   )
 }

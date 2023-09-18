265ac0ec232133dbbef7ff1abc6e41538616096f
JL-OLEMAR <joseluis19963@gmail.com>
2023-09-18 09:55:17 -0500

    feat: Create a mongoose schema

---
+++ models/entry.ts
@@ -0,0 +1,20 @@
+import mongoose, { Model, Schema } from 'mongoose'
+import { Entry } from '../interfaces'
+
+interface IEntry extends Entry { }
+
+const entrySchema = new Schema({
+  description: { type: String, required: true },
+  createdAt: { type: Number },
+  status: {
+    type: String,
+    enum: {
+      values: ['todo', 'in-progress', 'done'],
+      message: '{VALUE} is not supported'
+    }
+  }
+})
+
+const EntryModel: Model<IEntry> = mongoose.models.Entry || mongoose.model('Entry', entrySchema)
+
+export default EntryModel
